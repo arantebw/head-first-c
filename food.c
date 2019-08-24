99042b3d3e386811d8fe399658d328d9ea0dabf4
Billy Arante <arantebillywilson@gmail.com>
2019-08-24 13:36:19 +0800

    update: food.c

--- food.c
+++ food.c
@@ -4,7 +4,7 @@ int main()
 {
     char food[5];
     printf("Enter favorite food: ");
-    scanf("%s", food);
+    fgets(food, sizeof(food), stdin);
     printf("Favorite food: %s\n", food);
     return 0;
 }
